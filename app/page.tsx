59a68ba8c5df90a72e195271260f61271304eab3
Anthony Nguyen <anthonynguyen@Anthonys-Personal-Macbook.local>
2024-03-27 18:19:23 -0700

    added tiles to home page and made them links

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,30 @@ export default function Home() {
   return (
     <main className="">
       <Shell pageURL="/" title="Summary">
-        <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
           <StatCard
             name="Total Cases"
             value={summarizedToDates.cases.total_cases_repo}
             color="text-red-500"
+            href="/cases"
           />
           <StatCard
             name="Total Deaths"
             value={summarizedToDates.deaths.total_dth_date}
             color="text-red-500"
+            href="/deaths"
+          />
+                    <StatCard
+            name="% Population Vaccination"
+            value={summarizedToDates.totalDoses.oc_pop_fully_vaxed}
+            color="text-green-500"
+            href="/vaccinations"
+          />
+                    <StatCard
+            name="Peak ICU Hospitalizations"
+            value={summarizedToDates.hospitalized.peakICU}
+            color="text-orange-500"
+            href="/hospitalizations"
           />
         </div>
       </Shell>
--- components/ui/StatCard.tsx
+++ components/ui/StatCard.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import Card from "./Card";
 import classNames from "@/utils/makeClassNames";
+import Link from "next/link";
 
 type Props = {
   name: string;
   value: number | string;
   color?: string;
+  href?: string;
 };
 
-export default function StatCard({ name, value, color }: Props) {
+export default function StatCard({ name, value, color, href }: Props) {
   return (
-    <Card>
-      <dt className="truncate text-sm font-medium text-gray-500">{name}</dt>
-      <dd
-        className={classNames(
-          "mt-1 text-3xl font-semibold tracking-tight",
-          color
-        )}
-      >
-        {value.toLocaleString()}
-      </dd>
-    </Card>
+    <Link href={href ? href : ""}>
+      <Card>
+        <dt className="truncate text-sm font-medium text-gray-500">{name}</dt>
+        <dd
+          className={classNames(
+            "mt-1 text-3xl font-semibold tracking-tight",
+            color
+          )}
+        >
+          {value.toLocaleString()}
+        </dd>{" "}
+      </Card>
+    </Link>
   );
 }
